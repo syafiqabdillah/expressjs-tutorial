50069c2b30c0d29255b4ac38102c6329c3b9c90c
syafiq <abdillah.syafiq@gmail.com>
2021-05-25 15:56:57 +0800

    nested route for modularity

--- routes/api.js
+++ routes/api.js
@@ -1,25 +1,6 @@
 const express = require("express");
 const router = express.Router()
 
-// get list of ninjas from database 
-router.get('/ninjas', function(req, res) {
-  res.send({ type: 'GET' })
-})
-
-// add a ninja to database 
-router.post('/ninjas', function(req, res) {
-  console.log(req.body)
-  res.send({ type: 'POST', body: req.body })
-})
-
-// update a ninja to database
-router.put('/ninjas/:id', function(req, res) {
-  res.send({ type: 'PUT', id: req.params.id })
-})
-
-// delete a ninja from database 
-router.delete('/ninjas/:id', function(req, res) {
-  res.send({ type: 'DELETE' })
-})
+router.use('/ninjas', require('./ninjas'))
 
 module.exports = router
